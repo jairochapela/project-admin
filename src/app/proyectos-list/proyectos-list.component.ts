05e266b74b3b9e208c7a4b5ee77449a369b4aeba
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-08-11 20:43:06 +0200

    Consulta de proyectos a la API

--- src/app/proyectos-list/proyectos-list.component.ts
+++ src/app/proyectos-list/proyectos-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-proyectos-list',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proyectos-list.component.css']
 })
 export class ProyectosListComponent implements OnInit {
+  proyectos = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any[]>("http://localhost:3000/api/proyectos")
+    .subscribe(proyectos => {
+      this.proyectos = proyectos;
+    })
   }
 
 }
