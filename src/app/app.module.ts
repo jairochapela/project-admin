0a7157c6020cb1b77cfce2bfd98cd938f0124f81
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-08-11 18:26:55 +0200

    Creación de componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProyectosListComponent } from './proyectos-list/proyectos-list.component';
+import { TareasListComponent } from './tareas-list/tareas-list.component';
+import { UsuariosListComponent } from './usuarios-list/usuarios-list.component';
+import { RolesListComponent } from './roles-list/roles-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProyectosListComponent,
+    TareasListComponent,
+    UsuariosListComponent,
+    RolesListComponent
   ],
   imports: [
     BrowserModule,
