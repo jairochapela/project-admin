1b7fd2365a35cff4be15f4d9f37693f1456b3fd2
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-08-11 18:34:42 +0200

    Rutas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProyectosListComponent } from './proyectos-list/proyectos-list.component';
+import { TareasListComponent } from './tareas-list/tareas-list.component';
+import { UsuariosListComponent } from './usuarios-list/usuarios-list.component';
+import { RolesListComponent } from './roles-list/roles-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'proyectos', component: ProyectosListComponent},
+  {path: 'tareas', component: TareasListComponent},
+  {path: 'usuarios', component: UsuariosListComponent},
+  {path: 'roles', component: RolesListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
