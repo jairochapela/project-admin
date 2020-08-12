05fa466a2293ed5051a648f175307739cd88772e
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-08-12 17:19:10 +0200

    Roles y usuarios.

--- src/app/roles-list/roles-list.component.ts
+++ src/app/roles-list/roles-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-roles-list',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./roles-list.component.css']
 })
 export class RolesListComponent implements OnInit {
+  roles: any[];
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any[]>("http://localhost:3000/api/roles")
+    .subscribe(roles => {
+      this.roles = roles;
+    })
   }
 
 }
--- src/app/usuarios-list/usuarios-list.component.ts
+++ src/app/usuarios-list/usuarios-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-usuarios-list',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuarios-list.component.css']
 })
 export class UsuariosListComponent implements OnInit {
+  usuarios: any[];
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any[]>("http://localhost:3000/api/usuarios")
+    .subscribe(usuarios => {
+      this.usuarios = usuarios;
+    })
   }
 
 }
